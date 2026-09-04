639a74f4143ba5e444641ae45d71a999079894fc
Karl Koor <karl.koor@voco.ee>
2026-09-04 10:19:00 +0300

    essa ülessanne

---
+++ praks1/praks3/yl1.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+#skript küsib ees ja perenime, seejärel tervitab sind echodes sinu ees ja perenime
+echo -n "Sisesta oma ees- ja perenimi: "
+read eesnimi perenimi
+echo "Tere tulemast, $eesnimi $perenimi"
