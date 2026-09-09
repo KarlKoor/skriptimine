33c8541ac92a07974f5fec8adc5664b65aae1d98
Karl Koor <karl.koor@voco.ee>
2026-09-09 09:14:16 +0300

    tunnitöö

---
+++ userinput/cars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Mis autod sulle meeldivad?
+
+read car1 car2 car3 car4
+echo Sinu esimene auto, mis sulle meeldis oli: $car1
+echo Sinu teine auto, mis sulle meeldis oli: $car2
+echo Sinu kolmas auto, mis sulle meeldis oli: $car3
+echo Sinu neljas auto, mis sulle meeldis oli: $car4
