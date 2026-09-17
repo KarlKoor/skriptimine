8570cd4198bc39e214cf1a2321b9c6d7514eb46f
Karl Koor <karl.koor@voco.ee>
2026-09-17 11:33:56 +0300

    vanad praksid

---
+++ praks2/yl1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -n "tere, "
+
+kasutaja="Karl Koor"
+echo -n "$kasutaja"
+
+echo "!"
---
+++ praks2/yl2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+date +"Today is %A, %B %d, %Y"
+
+date +"Time is %H:%M"
+echo
+date +"Calendar of %B %Y"
+cal | tail -n 7
